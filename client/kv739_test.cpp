2e4d5a5cfc8337462ee6284b8ce434fc8aa3a630
Huai-Dan Chang <hchang256@wisc.edu>
2024-09-29 21:59:03 -0500

    add performance test

--- client/kv739_test.cpp
+++ client/kv739_test.cpp
@@ -3,6 +3,9 @@
 #include <chrono>
 #include <string>
 #include <cstdlib> // For getenv
+#include <vector>
+#include <random>
+#include <functional>
 
 // Include your client header file
 #include "kv739_client.h"
@@ -123,6 +126,101 @@ void test_concurrent_puts()
     print_test_result("Concurrent Puts Operation", passed);
 }
 
+// Helper function to measure and print latency and throughput
+void measure_performance(const std::string &test_name, int num_requests, const std::function<void()> &test_function)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < num_requests; ++i)
+    {
+        test_function();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    double total_time = duration.count();
+    double latency = total_time / num_requests;
+    double throughput = num_requests / total_time;
+
+    std::cout << "Performance Test: " << test_name << std::endl;
+    std::cout << "Total Time: " << total_time << " seconds" << std::endl;
+    std::cout << "Latency: " << latency * 1000 << " ms/request" << std::endl;
+    std::cout << "Throughput: " << throughput << " requests/second" << std::endl;
+}
+
+// Test: Performance with 10% hot keys and 90% of requests to those hot keys
+void test_hot_keys_performance(int num_requests)
+{
+    std::cout << "Running performance test: 10% Hot Keys" << std::endl;
+
+    // Prepare hot keys (10% of total)
+    int num_hot_keys = 10;
+    std::vector<std::string> hot_keys;
+    char old_value[1024] = {0};
+    for (int i = 0; i < num_hot_keys; ++i)
+    {
+        hot_keys.push_back("hot_key_" + std::to_string(i));
+        kv739_put(const_cast<char *>(hot_keys[i].c_str()), "initial_hot_value", old_value);
+    }
+
+    // Prepare cold keys (remaining 90%)
+    int num_cold_keys = 90;
+    std::vector<std::string> cold_keys;
+    for (int i = 0; i < num_cold_keys; ++i)
+    {
+        std::string cold_key = "cold_key_" + std::to_string(i);
+        cold_keys.push_back(cold_key);
+        kv739_put(const_cast<char *>(cold_key.c_str()), "initial_cold_value", old_value);
+    }
+
+    // Randomly generate 90% of requests for hot keys and 10% for cold keys
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<> dist(0, 99); // Generate numbers between 0 and 99
+
+    measure_performance("10% Hot Keys Performance", num_requests, [&]()
+    {
+        std::string key;
+        if (dist(gen) < 90) // 90% of the time, pick a hot key
+        {
+            key = hot_keys[dist(gen) % num_hot_keys];
+        }
+        else // 10% of the time, pick a random cold key
+        {
+            key = cold_keys[dist(gen) % num_cold_keys];
+        }
+        char value[1024] = {0};
+        kv739_get(const_cast<char *>(key.c_str()), value);
+    });
+}
+
+// Test: Performance with uniformly distributed keys and requests
+void test_uniform_distribution_performance(int num_requests)
+{
+    std::cout << "Running performance test: Uniform Distribution" << std::endl;
+
+    // Prepare a larger number of uniformly distributed keys
+    int num_keys = 100;
+    std::vector<std::string> keys;
+    char old_value[1024] = {0};
+    for (int i = 0; i < num_keys; ++i)
+    {
+        keys.push_back("uniform_key_" + std::to_string(i));
+        kv739_put(const_cast<char *>(keys[i].c_str()), "initial_uniform_value", old_value);
+    }
+
+    // Randomly pick keys for each request
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<> dist(0, num_keys - 1);
+
+    measure_performance("Uniform Distribution Performance", num_requests, [&]()
+    {
+        std::string key = keys[dist(gen)];
+        char value[1024] = {0};
+        kv739_get(const_cast<char *>(key.c_str()), value);
+    });
+}
+
 int main()
 {
     // Read the server address from the environment variable "SERVER_ADDRESS"
@@ -140,6 +238,11 @@ int main()
     test_put_overwrite();
     test_get_non_existent_key();
     test_concurrent_puts();
+
+    // Performance tests
+    int num_requests = 10000;
+    test_hot_keys_performance(num_requests);
+    test_uniform_distribution_performance(num_requests);
 
     // Shutdown client after tests
     shutdown_client();
